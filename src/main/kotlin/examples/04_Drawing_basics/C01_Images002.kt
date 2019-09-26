ef085f0d1d2d46fe1581e998e2086be76b00d974
Edwin Jakobs <edwin@rndr.studio>
2019-09-26 15:39:18 +0200

    Update examples

--- src/main/kotlin/examples/04_Drawing_basics/C01_Images002.kt
+++ src/main/kotlin/examples/04_Drawing_basics/C01_Images002.kt
@@ -18,7 +18,6 @@ fun main(args: Array<String>) {
             val image = loadImage("data/cheeta.jpg")
         
             extend {
-                val a = 5
                 val areas = (0..10).flatMap { y ->
                     (0..10).map { x ->
                         val source = Rectangle(x * (width / 10.0), y * (height / 10.0), width / 5.0, height / 5.0)
--- src/main/kotlin/examples/11_Advanced_Topics/C06_Compute_shaders000.kt
+++ src/main/kotlin/examples/11_Advanced_Topics/C06_Compute_shaders000.kt
@@ -2,16 +2,29 @@
 package examples.`11_Advanced_Topics`
 
 import org.openrndr.application
+import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.ComputeShader
+import org.openrndr.draw.ImageAccess
+import org.openrndr.draw.colorBuffer
+import org.openrndr.draw.loadImage
+import org.openrndr.extensions.SingleScreenshot
 import java.io.File
 
 fun main(args: Array<String>) {
     application {
         program {
             val cs = ComputeShader.fromFile(File("data/compute-shaders/fill.cs"))
+            val result = colorBuffer(width, height)
+            val testRGBa = colorBuffer(640, 480)
+            val test = loadImage("data/images/cheeta.jpg")
+            test.copyTo(testRGBa)
         
-    
             extend {
+                cs.uniform("fill", ColorRGBa.PINK.shade(0.1))
+                cs.image("inputImg", 1, testRGBa, ImageAccess.READ)
+                cs.image("outputImg", 0, result, ImageAccess.WRITE)
+                cs.execute(result.width, result.height, 1)
+                drawer.image(result)
             }
         }
     }
