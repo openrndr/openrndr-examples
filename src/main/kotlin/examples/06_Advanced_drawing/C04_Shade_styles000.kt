2ea02ab57dc466c71d88d59b9a32bae95af5a56b
Edwin Jakobs <edwin@rndr.studio>
2019-10-26 15:16:27 +0200

    Update examples

--- src/main/kotlin/examples/06_Advanced_drawing/C04_Shade_styles000.kt
+++ src/main/kotlin/examples/06_Advanced_drawing/C04_Shade_styles000.kt
@@ -3,9 +3,15 @@ package examples.`06_Advanced_drawing`
 
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.MagnifyingFilter
+import org.openrndr.draw.MinifyingFilter
+import org.openrndr.draw.loadImage
 import org.openrndr.draw.shadeStyle
 import org.openrndr.extensions.SingleScreenshot
 import org.openrndr.ffmpeg.ScreenRecorder
+import org.openrndr.shape.Circle
+import kotlin.math.cos
+import kotlin.math.sin
 
 fun main(args: Array<String>) {
     application {
--- src/main/kotlin/examples/06_Advanced_drawing/C04_Shade_styles003.kt
+++ src/main/kotlin/examples/06_Advanced_drawing/C04_Shade_styles003.kt
@@ -3,24 +3,36 @@ package examples.`06_Advanced_drawing`
 
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.MagnifyingFilter
+import org.openrndr.draw.MinifyingFilter
+import org.openrndr.draw.loadImage
 import org.openrndr.draw.shadeStyle
 import org.openrndr.extensions.SingleScreenshot
 import org.openrndr.ffmpeg.ScreenRecorder
+import org.openrndr.shape.Circle
+import kotlin.math.cos
+import kotlin.math.sin
 
 fun main(args: Array<String>) {
     application {
         program {
+            val image = loadImage("data/images/cheeta.jpg")
+            image.filter(MinifyingFilter.LINEAR_MIPMAP_NEAREST, MagnifyingFilter.LINEAR)
             extend {
                 drawer.shadeStyle = shadeStyle {
                     fragmentTransform = """
-                        | float c = cos(c_screenPosition.x * 0.1 + p_time) * 0.5 + 0.5;
-                        | x_fill.rgb *= vec3(c, c, c);
-                        """.trimMargin()
-                    parameter("time", seconds)
+                            vec2 texCoord = c_boundsPosition.xy;
+                            texCoord.y = 1.0 - texCoord.y;
+                            vec2 size = textureSize(p_image, 0);
+                            texCoord.x /= size.x/size.y;
+                            x_fill = texture(p_image, texCoord);
+                        """
+                    parameter("image", image)
                 }
-                drawer.fill = ColorRGBa.PINK
-                drawer.stroke = null
-                drawer.rectangle(width / 2.0 - 200.0, height / 2.0 - 200.0, 400.0, 400.0)
+            
+                val shape = Circle(width / 2.0, height / 2.0, 110.0).shape
+                drawer.translate(cos(seconds) * 100.0, sin(seconds) * 100.0)
+                drawer.shape(shape)
             }
         }
     }
