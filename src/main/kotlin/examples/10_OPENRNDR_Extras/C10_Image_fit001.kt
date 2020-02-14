83f1a0731cf96891dc7fa50fa91c38cdfccb8cc7
Edwin Jakobs <edwin@rndr.studio>
2020-02-14 17:13:02 +0100

    Update examples

---
+++ src/main/kotlin/examples/10_OPENRNDR_Extras/C10_Image_fit001.kt
@@ -0,0 +1,28 @@
+
+package examples.`10_OPENRNDR_Extras`
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.loadImage
+import org.openrndr.extras.imageFit.FitMethod
+import org.openrndr.extras.imageFit.imageFit
+import org.openrndr.ffmpeg.ScreenRecorder
+import kotlin.math.PI
+import kotlin.math.cos
+
+fun main(args: Array<String>) {
+    application {
+        program {
+            val image = loadImage("data/images/cheeta.jpg")
+            extend {
+                val margin = 100.0
+                drawer.imageFit(image, 20.0, 20.0 + margin / 2, width - 40.0, height - 40.0 - margin, FitMethod.Contain, horizontalPosition = cos(seconds) * 1.0)
+                // -- illustrate the placement rectangle
+                drawer.fill = null
+                drawer.stroke = ColorRGBa.WHITE
+                drawer.rectangle(20.0, 20.0 + margin / 2.0, width - 40.0, height - 40.0 - margin)
+            }
+        }
+    }
+}
+    
